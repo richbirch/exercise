5e90429b20cc65e7855f492c4dca03b6b650c259
Rich Birch <ringo@albumsnaps.com>
2017-03-18 17:04:39 +0000

    Add tests for multiple fruit of different types

--- src/test/scala/CheckoutSpec.scala
+++ src/test/scala/CheckoutSpec.scala
@@ -1,20 +1,50 @@
 import org.scalatest._
 
 object checkout {
-  def totalCost(items: List[String]): Double = 0
+  def totalCost(items: List[String]): Double = items.map {
+    case "apple" => 0.6
+    case "orange" => 0.25
+  }.sum
 }
 
 class CheckoutSpec extends FlatSpec {
-
-  def testPrices: String => Double = {
-    case "apple" => 0.6
-    case "orange" => 0.25
-  }
 
   "checkout" should "return 0 when given an empty list" in {
     val items = List()
     val actualTotal = checkout.totalCost(items)
     val expectedTotal = 0
+
+    assert(actualTotal === expectedTotal)
+  }
+
+  "checkout" should "return 0.6 when given a list containing a single apple" in {
+    val items = List("apple")
+    val actualTotal = checkout.totalCost(items)
+    val expectedTotal = 0.6
+
+    assert(actualTotal === expectedTotal)
+  }
+
+  "checkout" should "return 1.2 when given a list of two apples" in {
+    val items = List("apple", "apple")
+    val actualTotal = checkout.totalCost(items)
+    val expectedTotal = 1.2
+
+    assert(actualTotal === expectedTotal)
+  }
+
+  "checkout" should "return 0.25 when given a list containing one orange" in {
+    val items = List("orange")
+    val actualTotal = checkout.totalCost(items)
+    val expectedTotal = 0.25
+
+    assert(actualTotal === expectedTotal)
+  }
+
+  "checkout" should "return 1.7 when given two apples and two oranges" in {
+    val items = List("apple", "apple", "orange", "orange")
+    val actualTotal = checkout.totalCost(items)
+    val expectedTotal = 1.7
 
     assert(actualTotal === expectedTotal)
   }
